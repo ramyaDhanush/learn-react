b559de74716cdb7ee8797cecd143c1276956741c
RamyaDhanush <ramyadhanush99@gmail.com>
2020-08-17 08:23:01 +0530

    Add : HTTP using axios
    Signed-off-by: RamyaDhanush <ramyadhanush99@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import "./App.css";
 import "./appStyles.css";
-import ComponentC from "./components/ComponentC";
-import { UserProvider } from "./components/userContext";
+import PostList from "./components/PostList";
 function App() {
   return (
     <div className="App">
-      <UserProvider value="Ramya" >
-        <ComponentC />
-      </UserProvider>
+      <PostList/>
     </div>
   );
 }
