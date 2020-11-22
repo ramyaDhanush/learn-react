6474ddd94e6fbaead7e42fa2f5ab92e27bcfe45f
Ramya D <ramyadhanush99@gmail.com>
2020-11-22 20:43:10 +0530

    Fix : TODO toggle useReducer hook
    Signed-off-by: Ramya D <ramyadhanush99@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,44 +1,56 @@
-import React, {useReducer, useState} from "react";
-import "./App.css";
-import "./appStyles.css";
+import React, { useReducer, useState } from "react";
 
-function reducer(state, action){
-  // Pure function
-  switch(action.type){
-    case 'add-todo':
-      // don't mutate state like state++, 
-      // rather return new state 'state+1'
-      return {todo: [...state.todo, {text:action.text, complete:false} ]};
-    case 'toggle-todo':
-      return {todo: [state.todo.map((t, idx)=> idx ===action.idx ?{...t, completed:!t.completed}:t)]}
+function reducer(state, action) {
+  switch (action.type) {
+    case "add-todo":
+      return {
+        todos: [...state.todos, { text: action.text, completed: false }],
+        todoCount: state.todoCount + 1
+      };
+    case "toggle-todo":
+      return {
+        todos: state.todos.map((t, idx) =>
+          idx === action.idx ? { ...t, completed: !t.completed } : t
+        ),
+        todoCount: state.todoCount
+      };
     default:
       return state;
   }
 }
 
 const App = () => {
-  const [{todo}, dispatch] = useReducer(reducer, {todo:[]});
-  const [text, setText] = useState('')
+  const [{ todos, todoCount }, dispatch] = useReducer(reducer, {
+    todos: [],
+    todoCount: 0
+  });
+  const [text, setText] = useState();
+
   return (
-    <>
-    <form onSubmit={e=>{
-      e.preventDefault();
-      dispatch({type:"add-todo", text});
-      setText("");
-    }}>
-      <input type="text" value={text} onChange={e => setText(e.target.value)}/>
-    </form>
-    {todo.map((t, idx)=>(
-      <div key={t.idx} onClick={()=>dispatch({type:'toggle-todo', idx})}
-        style={{textDecoration: t.completed ?'line-through':''}}>
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          dispatch({ type: "add-todo", text });
+          setText("");
+        }}
+      >
+        <input value={text} onChange={e => setText(e.target.value)} />
+      </form>
+      <div>number of todos: {todoCount}</div>
+      {todos.map((t, idx) => (
+        <div
+          key={t.text}
+          onClick={() => dispatch({ type: "toggle-todo", idx })}
+          style={{
+            textDecoration: t.completed ? "line-through" : ""
+          }}
+        >
           {t.text}
-      </div>))}
-    {/* <pre>
-      {JSON.stringify(todo, null, 2)}
-    </pre> */}
-    </>
+        </div>
+      ))}
+    </div>
   );
-}
+};
 
 export default App;
- 
