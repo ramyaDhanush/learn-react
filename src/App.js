f50179cfbb3bd6a6e44782e96e9623a9cd9b171a
Ramya D <ramyadhanush99@gmail.com>
2020-11-22 20:40:59 +0530

    Add : useReducer Todo hook
    Signed-off-by: Ramya D <ramyadhanush99@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
-import React, {useReducer} from "react";
+import React, {useReducer, useState} from "react";
 import "./App.css";
 import "./appStyles.css";
 
 function reducer(state, action){
   // Pure function
   switch(action.type){
-    case 'increment':
+    case 'add-todo':
       // don't mutate state like state++, 
       // rather return new state 'state+1'
-      return state+1;
-    case 'decrement':
-      return state-1;
+      return {todo: [...state.todo, {text:action.text, complete:false} ]};
+    case 'toggle-todo':
+      return {todo: [state.todo.map((t, idx)=> idx ===action.idx ?{...t, completed:!t.completed}:t)]}
     default:
       return state;
   }
 }
 
 const App = () => {
-  const [count, dispatch] = useReducer(reducer, 0);
+  const [{todo}, dispatch] = useReducer(reducer, {todo:[]});
+  const [text, setText] = useState('')
   return (
     <>
-    <h1>{count}</h1>
-    <button onClick={()=> dispatch({type:'increment'})}>Increment</button>
-    <button onClick={()=> dispatch({type:'decrement'})}>Decrement</button>
-    
+    <form onSubmit={e=>{
+      e.preventDefault();
+      dispatch({type:"add-todo", text});
+      setText("");
+    }}>
+      <input type="text" value={text} onChange={e => setText(e.target.value)}/>
+    </form>
+    {todo.map((t, idx)=>(
+      <div key={t.idx} onClick={()=>dispatch({type:'toggle-todo', idx})}
+        style={{textDecoration: t.completed ?'line-through':''}}>
+          {t.text}
+      </div>))}
+    {/* <pre>
+      {JSON.stringify(todo, null, 2)}
+    </pre> */}
     </>
   );
 }
